817400469e68e39878eafd4f8b9129bb69a7cc50
Ammar2882 <ammar.cnation@gmail.com>
2022-05-30 11:55:52 -0700

    berbix after verification

--- src/constants/axiosInstance.js
+++ src/constants/axiosInstance.js
@@ -1,5 +1,5 @@
 //https://cannabis-server.herokuapp.com
 import axios from 'axios'
 export const axiosInstance = axios.create({
-    baseURL: 'https://cannabis-server.herokuapp.com',
+    baseURL: 'https://06cd-206-84-154-165.ap.ngrok.io',
 });
