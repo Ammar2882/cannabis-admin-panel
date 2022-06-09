4a651450f64cf2d181d1e50640ada6133cdb6540
Ammar2882 <ammar.cnation@gmail.com>
2022-06-09 12:04:54 -0700

    berbix

--- src/constants/axiosInstance.js
+++ src/constants/axiosInstance.js
@@ -1,5 +1,5 @@
 //https://cannabis-server.herokuapp.com
 import axios from 'axios'
 export const axiosInstance = axios.create({
-    baseURL: 'https://367c-206-84-154-165.ap.ngrok.io',
+    baseURL: 'https://3d29-206-84-154-165.ap.ngrok.io',
 });
